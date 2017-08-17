41c2ad10c6d53ab9543db6fb203602b650685602
Carlos Telis <carlos.telis@outlook.com>
2017-08-16 23:36:22 -0500

    Add todos and id

--- server.js
+++ server.js
@@ -1,10 +1,42 @@
 var express = require('express');
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
+let todos = [{
+    id: 1,
+    descripcion: 'Ver a mamá para almorzar',
+    completado: false,
+}, {
+    id: 2,
+    description: 'Ir al mercado',
+    completado: false
+}, {
+    id: 3,
+    description: 'Trabajar',
+    completado: true
+}];
 
 app.get('/', function(req, res) {
     res.send('Todo API root');
+});
+
+app.get('/todos', function(req, res) {
+    // json() se encarga de decidir si usar parse o stringify para enviar
+    res.json(todos);
+});
+
+app.get('/todos/:id', function(req, res) {
+    console.log(`Pidiendo ${req.params.id}`);
+
+    todos.forEach(function(todo) {
+        // El parámetro viene como tipo string
+        if (todo.id === parseInt(req.params.id, 10)) {
+            res.json(todo);
+        }
+    });
+
+    // Recurso no encontrado
+    res.status(404).send();
 });
 
 app.listen(PORT, function() {
